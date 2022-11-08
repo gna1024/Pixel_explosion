f2676dc5e0fe6f43bcc81549c54ffe1ddbe4b457
gna1024 <nikitahoncharov1024@gmail.com>
2022-11-08 11:25:43 +0200

    Particle class

--- script.js
+++ script.js
@@ -3,4 +3,28 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = this.window.innerHeight; 
+
+    const image1 = document.getElementById('image1');
+
+    class Particle {
+        constructor(){
+            this.x = 100;
+            this.y = 100;
+            this.size = 400;
+        }
+        draw(){
+            ctx.fillRect(this.x, this.y, this.size, this.size);
+        }
+    }
+
+    class Effect {
+
+    }
+
+    const particle1 = new Particle();
+    particle1.draw();
+
+    function animate(){
+
+    }
 });
