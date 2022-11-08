1d88f5c5b36cf18e183f8fdf726e4d8a8cd6cb90
gna1024 <nikitahoncharov1024@gmail.com>
2022-11-08 13:11:10 +0200

    Pixel extracting

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ window.addEventListener('load', function(){
             this.effect = effect;
             this.x = Math.random() * this.effect.width;
             this.y = Math.random() * this.effect.height;
+            this.originX = Math.floor(x);
+            this.originY = Math.floor(y);
             this.size = 5;
             this.vx = Math.random() - 0.5;
             this.vy = Math.random() - 0.5;
@@ -32,11 +34,24 @@ window.addEventListener('load', function(){
             this.CenterY = this.height * 0.5;
             this.x = this.CenterX - this.image.width * 0.5;
             this.y = this.CenterY - this.image.height * 0.5;
+            this.gap = 5;
         }
         init(context){
             context.drawImage(this.image, this.x, this.y);
-            const pixels = context.getImageData(0, 0, this.width, this.height);
-            console.log(pixels)
+            const pixels = context.getImageData(0, 0, this.width, this.height).data;
+            for(let y = 0; y < this.height; y += this.gap){
+                for(let x = 0; x < this.width; x += this.gap){
+                    const index = (y * this.width + x) * 4;
+                    const red = pixels[index];
+                    const green = pixels[index + 1];
+                    const blue = pixels[index + 2];
+                    const alpha = pixels[index + 3];
+                    const color = 'rgb(' + red + "," + green + ',' + blue + ")";
+                    if (alpha == 0) {
+                        this.particlesArray.push(new Particle(this, x, y, color))
+                    }
+                }
+            }
         }
         draw(context){
             this.particlesArray.forEach(particle => particle.draw(context));
